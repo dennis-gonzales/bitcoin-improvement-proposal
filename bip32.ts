88fc775777de78c263de00b8c51cbf2856d0ccb9
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-03-03 17:11:09 +0800

    refactor

--- bip32.ts
+++ bip32.ts
@@ -1,7 +1,7 @@
 import * as bip32 from 'bip32';
 import { BIP32Interface } from 'bip32';
-let node: BIP32Interface = bip32.fromBase58('xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi');
 
+let node: BIP32Interface = bip32.fromBase58('xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi');
 let child: BIP32Interface = node.derivePath('m/0/0');
 
 console.log(child);
--- bip39.ts
+++ bip39.ts
@@ -1,19 +1,5 @@
-import { 
-    generateMnemonic, 
-    mnemonicToSeedSync,
-    validateMnemonic,
-    entropyToMnemonic,
-    mnemonicToEntropy,
-    wordlists
-} from 'bip39';
-
-// |  ENT  | CS | ENT+CS |  MS  |
-// +-------+----+--------+------+
-// |  128  |  4 |   132  |  12  |
-// |  160  |  5 |   165  |  15  |
-// |  192  |  6 |   198  |  18  |
-// |  224  |  7 |   231  |  21  |
-// |  256  |  8 |   264  |  24  |
+import * as bip39 from 'bip39';
+import { wordlists } from 'bip39';
 
 const entropyLength = [128, 160, 192, 224, 256];
 const strength = entropyLength[4];
@@ -21,7 +7,7 @@ const strength = entropyLength[4];
 /** Generate Entropy, Mnemonic Words and Seed
 */
 console.log();
-const mnemonic = generateMnemonic(
+const mnemonic = bip39.generateMnemonic(
     strength, 
     null, 
     wordlists.EN);
@@ -34,22 +20,22 @@ console.log("numberOfWords: ", numberOfWords);
 console.log();
 
 
-const seed = mnemonicToSeedSync(mnemonic, "thicc thigs");
+const seed = bip39.mnemonicToSeedSync(mnemonic, "thicc thigs");
 console.log("seed: ", JSON.stringify(seed));
 console.log();
 
 
-const seedInHex = mnemonicToSeedSync(mnemonic, "thicc thigs").toString('hex');
+const seedInHex = bip39.mnemonicToSeedSync(mnemonic, "thicc thigs").toString('hex');
 console.log("seedInHex: ", JSON.stringify(seedInHex));
 console.log();
 
 
-const entropy = mnemonicToEntropy(mnemonic, wordlists.EN);
+const entropy = bip39.mnemonicToEntropy(mnemonic, wordlists.EN);
 console.log("entropy: ", entropy);
 console.log();
 
 
-const isValid = validateMnemonic(mnemonic);
+const isValid = bip39.validateMnemonic(mnemonic);
 console.log("isValid: ", isValid);
 console.log();
 
@@ -58,11 +44,19 @@ console.log("--------------------------------------");
 
 /** Retrieve Entropy, Mnemonic Words and Seed
 */
-const retievedMnemonic = entropyToMnemonic(entropy);
+const retievedMnemonic = bip39.entropyToMnemonic(entropy);
 console.log("retievedMnemonic: ", retievedMnemonic);
 console.log();
 
+// references
+// |  ENT  | CS | ENT+CS |  MS  |
+// +-------+----+--------+------+
+// |  128  |  4 |   132  |  12  |
+// |  160  |  5 |   165  |  15  |
+// |  192  |  6 |   198  |  18  |
+// |  224  |  7 |   231  |  21  |
+// |  256  |  8 |   264  |  24  |
 
-// refenrences
+// sources
 // https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
 // https://github.com/bitcoinjs/bip39
