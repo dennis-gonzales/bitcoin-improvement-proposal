37937e620f305cde06f962916fa85d263a0a4072
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-03-04 08:52:09 +0800

    added comments

--- bip32.ts
+++ bip32.ts
@@ -25,11 +25,50 @@ const hd: hdkey = hdkey.fromMasterSeed(seed);
 const privateExtendedKey: Buffer = hd.privateExtendedKey();
 const publicExtendedKey: Buffer = hd.publicExtendedKey();
 
+/**
+ * BIP32 explains how master keys and master chain code:
+ * are created from BIP 39 seed
+ * 
+ * The chain code is used as entropy in the Child Key Derivation(CKD) Function
+ * 
+ * Child Key Derivation(CKD) Function = HMAC SHA512 hash function:
+ * input: (index_no, chain_code, parent priv/pub key)
+ * output:
+ *   hash left
+ *   hash right (chain code)
+ * 
+ * Normal keys(index): i = [0, 2**31-1] => usage (Public Key)
+ * Hardened keys(index): i = [2**31, 2**32-1]  => usage (Private Key)
+ * 
+ * H => Child Private Key
+ * input: (parent priv key, hash left)
+ * output: child pub key
+ * 
+ * N => Child Public Key
+ * input (parent pub key, hash left)
+ */
 console.log("seed(BIP39 Seed): ", seed.toString("hex"));
 console.log();
+
+/**
+ * Entented Private Key (xprv) = 
+ * parent private key + parent chain code
+ * 
+ * Can create a complete branch with child private keys and child public keys
+ */
 console.log("privateExtendedKey(BIP32 Root Key): ", privateExtendedKey);
 console.log();
+
+/**
+ * Entented Pub Key (xpub) = 
+ * parent public key + parent chain code
+ * 
+ * Creates only a branch of child public keys.
+ * 
+ * Cannot create hardened keys.
+ */
 console.log("publicExtendedKey(BIP32 Root Key):", publicExtendedKey);
+console.log();
 
 // sources
 // https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
